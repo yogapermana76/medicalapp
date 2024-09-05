26c4b1b6d705648d535db84de28ee1b1eaa9c7f6
Yoga Permana <permana76.yp@gmail.com>
2024-09-05 19:43:59 +0700

    feat: widget - medicine card

---
+++ lib/widgets/medicine_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:medicalapp/theme.dart';
+
+class MedicineCard extends StatelessWidget {
+  const MedicineCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 12,
+        bottom: 14,
+        right: 20,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: backgroundColor4,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset('assets/images/obat_batuk.jpg', width: 60),
+          ),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Obat Batuk Anak',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  'Rp. 20.000',
+                  style: priceTextStyle,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
