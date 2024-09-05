d55a0c3f40be46aacd7e8deed3099dd71c992b2d
Yoga Permana <permana76.yp@gmail.com>
2024-09-05 18:21:06 +0700

    feat: widget - product card

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:medicalapp/theme.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 215,
+      height: 278,
+      margin: EdgeInsets.only(right: defaultMargin),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color(0xffECEDEF),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 30),
+          Image.asset(
+            'assets/images/obat_batuk.png',
+            width: 215,
+            height: 150,
+            fit: BoxFit.contain,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'COURT VISION 2.0',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  'Rp. 15.000',
+                  style: priceTextStyle.copyWith(fontWeight: medium),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
