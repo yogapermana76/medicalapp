6bfdeb6003239a49dd54194c470bb58abb3e1826
Yoga Permana <permana76.yp@gmail.com>
2024-09-06 10:18:46 +0700

    chore: user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,31 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String role;
+  late final String? token;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.role,
+    this.token,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        role: json['role'],
+      );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'role': role,
+    };
+  }
+}
