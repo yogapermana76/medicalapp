68240dc808e0ffbb3f180bd6359f6e9952f379ea
Yoga Permana <permana76.yp@gmail.com>
2024-09-07 22:41:28 +0700

    chore: service - add service chat

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:medicalapp/models/message_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:medicalapp/utils/error.utils.dart';
+import 'package:medicalapp/utils/local_torage.utils.dart';
+
+class ChatService {
+  final String baseUrl = 'http://127.0.0.1:3000/api';
+
+  Future createChat({
+    required String userId,
+    required String doctorId,
+  }) async {
+    final url = '$baseUrl/chat';
+
+    final String? token = await getString('token');
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': token as String,
+    };
+    final body = jsonEncode({'user_id': userId, 'doctor_id': doctorId});
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      throw extractErrorMessage(response.body);
+    }
+  }
+
+  Future<List<MessageModel>> getMessages({
+    required int chatId,
+  }) async {
+    final url = '$baseUrl/messages/$chatId';
+
+    final String? token = await getString('token');
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': token as String,
+    };
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)['data'] as List<dynamic>;
+      return data
+          .map((item) => MessageModel.fromJson(item as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw extractErrorMessage(response.body);
+    }
+  }
+}
