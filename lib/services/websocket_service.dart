4b87a62a8dc0ca443899dd06de975829474ab78b
Yoga Permana <permana76.yp@gmail.com>
2024-09-07 19:40:25 +0700

    chore: service - improve websocket service

--- lib/services/websocket_service.dart
+++ lib/services/websocket_service.dart
@@ -8,35 +8,58 @@ class WebSocketService {
 
   void connect() {
     socket = io.io(
-        'http://localhost:3000',
-        io.OptionBuilder()
-            .setTransports(['websocket']) // for Flutter or Dart VM
-            .build());
+      'http://localhost:3000',
+      io.OptionBuilder()
+          .setTransports(['websocket']) // Use websocket transport
+          .enableReconnection() // Enable automatic reconnections
+          .setReconnectionDelay(5000) // Delay between reconnections
+          .build(),
+    );
 
     socket?.onConnect((_) {
       print('Connected to WebSocket server');
+    });
+
+    socket?.onReconnect((_) {
+      print('Reconnected to WebSocket server');
+    });
+
+    socket?.onReconnectError((error) {
+      print('Reconnection error: $error');
     });
 
     socket?.onDisconnect((_) {
       print('Disconnected from WebSocket server');
     });
 
-    socket?.on('message', (data) {
-      debugPrint(data.toString());
-      // final message = data.toString();
-      // messages.value = [...messages.value, message];
+    socket?.onError((error) {
+      print('Socket error: $error');
     });
+
+    socket?.on('message', (data) {
+      final message = MessageModel.fromJson(data); // Parse incoming message
+      messages.value = [...messages.value, message]; // Append new message
+    });
+  }
+
+  void joinChat(int chatId) {
+    if (socket != null && socket!.connected) {
+      socket!.emit('join_chat', {'chatId': chatId});
+    }
   }
 
   void sendMessage(int chatId, int senderId, String message) {
-    socket?.emit('send_message', {
-      'chat_id': chatId,
-      'sender_id': senderId,
-      'message': message,
-    });
+    if (socket != null && socket!.connected) {
+      socket!.emit('send_message', {
+        'chat_id': chatId,
+        'sender_id': senderId,
+        'message': message,
+      });
+    }
   }
 
   void disconnect() {
+    socket?.off('message'); // Remove message listener
     socket?.disconnect();
   }
 }
