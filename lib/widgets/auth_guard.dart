097e53fde5ba724c0a637d848ebb65257ba79076
Yoga Permana <permana76.yp@gmail.com>
2024-09-08 11:07:15 +0700

    feat: widget - add auth guard

---
+++ lib/widgets/auth_guard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:medicalapp/pages/login_page.dart';
+import 'package:provider/provider.dart';
+import 'package:medicalapp/providers/auth_provider.dart';
+
+class AuthGuard extends StatelessWidget {
+  final Widget child;
+
+  const AuthGuard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    final user = authProvider.user;
+
+    if (user.token != null) {
+      return child;
+    } else {
+      return const LoginPage();
+    }
+  }
+}
